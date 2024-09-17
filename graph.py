b09abf85966c4a1b872f332abadd1c21555cf618
divinejoshua <divineuche399@gmail.com>
2024-09-17 17:30:42 +0100

    Added graph.py

--- graph.py
+++ graph.py
@@ -1,19 +1,29 @@
+import os
 from llama_index.core import StorageContext, load_index_from_storage
 from llama_index.core.indices import PropertyGraphIndex
 from llama_index.core import SimpleDirectoryReader
+from decouple import config
+
+# Configurations
+os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
 
 
-
+storage_dir = "./graph_store"
+# Load documents and build index
 documents = SimpleDirectoryReader(
-    "../../examples/data/paul_graham"
+    "data"
 ).load_data()
 
-# create
+
+# create and Store the date in the Graph store 
 index = PropertyGraphIndex.from_documents(documents)
+index.storage_context.persist(storage_dir)
 
-# save
-index.storage_context.persist("./storage")
-
-# load
-storage_context = StorageContext.from_defaults(persist_dir="./storage")
+# Load the data from the vector store
+storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
 index = load_index_from_storage(storage_context)
+
+# Chat with the data
+chat_engine = index.as_chat_engine()
+response = chat_engine.chat("What is this document talking about?")
+print(response)
--- vector.py
+++ vector.py
@@ -7,21 +7,21 @@ from decouple import config
 os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
 
 
-storage = "./vector_store"
+storage_dir = "./vector_store"
 # Load documents and build index
 documents = SimpleDirectoryReader(
     "data"
 ).load_data()
 
-# Store the date in the vector store 
+#Create and Store the date in the vector store 
 index = VectorStoreIndex.from_documents(documents)
-index.storage_context.persist(persist_dir=storage)
+index.storage_context.persist(persist_dir=storage_dir)
 
 # Load the data from the vector store
-storage_context = StorageContext.from_defaults(persist_dir=storage)
+storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
 index = load_index_from_storage(storage_context)
 
 # Chat with the data
 chat_engine = index.as_chat_engine()
-response = chat_engine.chat("What happened in 1962")
+response = chat_engine.chat("What is this document talking about?")
 print(response)
